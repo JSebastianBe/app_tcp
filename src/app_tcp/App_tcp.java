375779e2fb3e95b4a645c1c235f2ea4937fe5449
JSebastianBe <sebastianbm97@gmail.com>
2024-08-13 00:25:41 -0500

    Cambio

--- src/app_tcp/App_tcp.java
+++ src/app_tcp/App_tcp.java
@@ -14,7 +14,7 @@ public class App_tcp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Holis");
+        System.out.println("Hola manpinta");
     }
     
 }
